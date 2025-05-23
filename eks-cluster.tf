6342c938cb95135d6d62f2c4f54131c806cdd5dc
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-05-23 10:27:35 +0530

    Minor typo and output changes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -15,7 +15,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2023_ARM_64_STANDARD"
-    instance_types         = ["t4g.medium"]
+    instance_types         = ["t4g.medium","m6g.medium"]
     vpc_security_group_ids = [aws_security_group.app_tes_worker_mgmt.id]
   }
 
--- outputs.tf
+++ outputs.tf
@@ -14,5 +14,56 @@ output "cluster_security_group_id" {
 }
 
 output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+  description = "ARN of the OIDC Provider for IRSA."
+  value       = module.eks.oidc_provider_arn
+}
+
+# Node Group Outputs
+output "eks_managed_node_groups" {
+  description = "EKS managed node groups configuration."
+  value       = module.eks.eks_managed_node_groups
+}
+
+output "eks_managed_node_groups_autoscaling_group_names" {
+  description = "Names of the EKS managed node groups ASGs."
+  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
+}
+
+# VPC Outputs
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs."
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs."
+  value       = module.vpc.public_subnets
+}
+
+# Cluster Access
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster."
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "aws_auth_configmap_yaml" {
+  description = "Formatted yaml output for aws-auth ConfigMap."
+  value       = module.eks.aws_auth_configmap_yaml
+}
+
+# Cluster Security
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster."
+  value       = module.eks.cluster_primary_security_group_id
+}
+
+# Node Security Groups
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS nodes."
+  value       = module.eks.node_security_group_id
 }
