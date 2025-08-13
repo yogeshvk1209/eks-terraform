3aea26453dcde705266aa04130e3178534b42c4e
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-08-13 17:54:57 +0530

    Changes to folders

--- eks-addons.tf
+++ eks-addons.tf
@@ -1,5 +1,16 @@
 # EKS Add-ons - Installed after self-managed nodes are ready
 
+# Wait for nodes to be ready before installing addons
+resource "time_sleep" "wait_for_nodes" {
+  count = var.enable_self_managed_node_groups && var.install_eks_addons ? 1 : 0
+  
+  depends_on = [
+    aws_autoscaling_group.self_managed_nodes
+  ]
+  
+  create_duration = "300s"  # Wait 5 minutes for nodes to join
+}
+
 # CoreDNS Add-on
 resource "aws_eks_addon" "coredns" {
   count = var.enable_self_managed_node_groups && var.install_eks_addons ? 1 : 0
@@ -10,7 +21,7 @@ resource "aws_eks_addon" "coredns" {
   resolve_conflicts        = "OVERWRITE"
   
   depends_on = [
-    aws_autoscaling_group.self_managed_nodes
+    time_sleep.wait_for_nodes
   ]
   
   tags = {
@@ -28,7 +39,7 @@ resource "aws_eks_addon" "vpc_cni" {
   resolve_conflicts        = "OVERWRITE"
   
   depends_on = [
-    aws_autoscaling_group.self_managed_nodes
+    time_sleep.wait_for_nodes
   ]
   
   tags = {
@@ -45,7 +56,7 @@ resource "aws_eks_addon" "ebs_csi" {
   resolve_conflicts        = "OVERWRITE"
   
   depends_on = [
-    aws_autoscaling_group.self_managed_nodes
+    time_sleep.wait_for_nodes
   ]
   
   tags = {
