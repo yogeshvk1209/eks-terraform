534d85bc0c0288f95e77739553d687eac1f088f1
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-08-18 15:19:18 +0530

    Tagging chanegs

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -27,14 +27,50 @@ module "eks" {
 
   enable_irsa = true
 
-  tags = {
-    cluster = "app_test"
-  }
+#  tags = {
+#    cluster = "app_test"
+#  }
 
   vpc_id = module.vpc.vpc_id
 
+################################################
+######## Self Managed Node Groups ##############
+################################################
+self_managed_node_groups = {
+    example = {
+      ami_type      = "AL2023_x86_64_STANDARD"
+      instance_type = "t3.medium"
 
+      min_size = 1
+      max_size = 2
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
+      desired_size = 1
 
+      # This is not required - demonstrates how to pass additional configuration to nodeadm
+      # Ref https://awslabs.github.io/amazon-eks-ami/nodeadm/doc/api/
+      #cloudinit_pre_nodeadm = [
+      #  {
+      #    content_type = "application/node.eks.aws"
+      #    content      = <<-EOT
+      #      ---
+      #      apiVersion: node.eks.aws/v1alpha1
+      #      kind: NodeConfig
+      #      spec:
+      #        kubelet:
+      #          config:
+      #            shutdownGracePeriod: 30s
+      #    EOT
+      #  }
+      #]
+    }
+  }
+
+  tags = local.tags
+
+################################################
+######## EKS Managed Node Groups ##############
+################################################
 #  eks_managed_node_group_defaults = {
 #    ami_type               = var.managed_node_ami_type
 #    instance_types         = ["t4g.medium","t4g.small"]
--- main.tf
+++ main.tf
@@ -8,12 +8,20 @@ data "aws_caller_identity" "current" {}
 
 locals {
   cluster_name = "apptest-eks-${random_string.suffix.result}"
+  tags = {
+    cluster    = "app_test"
+    Example    = local.name
+    GithubRepo = "terraform-aws-eks"
+    GithubOrg  = "terraform-aws-modules"
+  }
 }
 
 resource "random_string" "suffix" {
   length  = 4
   special = false
 }
+
+
 
 terraform {
   required_version = ">= 1.12.0"
